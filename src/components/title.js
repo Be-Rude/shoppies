39bd23807f17d848a069a4029e797aedb1c6d244
Be-Rude <bradruud@gmail.com>
2020-09-07 17:09:58 -0700

    add title bar logo

--- src/components/title.js
+++ src/components/title.js
@@ -1,11 +1,21 @@
 import React from "react"
 
+import "./component.css"
+import { Container, Row } from "react-bootstrap";
+
 export default function Title() {
 
   return (
+    <Container className="title">
+    <Row>
+        <img className="logo" src={require("../img/LogoMakr_9b61vY.png")} alt=""/>
+   
     <h2>
-      Welcome to the Shoppie Award Nominations!
+          Welcome to the Shoppie Award Nominations!
     </h2>
+      </Row>
+    </Container>
+   
   )
   
 }
